67e040a374c46b7152df2c8612102b5e8f80a3f1
Eduard Cot Ros <eduardcot@iMac-de-Eduard.local>
2020-07-31 16:19:10 +0200

    upload docs

---
+++ docs/precache-manifest.cb03cf20db2aee88a6663c7363882afd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407f59d66973c357be29628786d7362a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a31df8767001b1be0d3e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d2de2156efabdc1a9f79",
+    "url": "/static/js/2.15c7307e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15c7307e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a31df8767001b1be0d3e",
+    "url": "/static/js/main.cf4f1ef1.chunk.js"
+  },
+  {
+    "revision": "67f9e687d4dff5ff9b73",
+    "url": "/static/js/runtime-main.612e3f48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
